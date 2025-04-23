05ba0ac87cab9c7b469c1bfba52d8e13a821377a
Gabbot369 <edwingabquirong@gmail.com>
2025-04-23 21:10:26 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('uploadBtn').addEventListener('click', function() {
-    // Optional: Show a message or visual feedback here before redirect
-    alert('Redirecting in 2 seconds...');
-  
-    // Delay of 2000 milliseconds (2 seconds)
-    setTimeout(function() {
-    }, 2000);
-  });
